3d07a5141b7108680fa05502dee933be0ab84fd0
jeppeter <jeppeter@gmail.com>
2023-08-17 09:34:32 +0800

    to make boot to the display running but not ok for poweroff and reboot

--- config.py
+++ config.py
@@ -99,12 +99,10 @@ def write_file(s,outfile=None):
 
 def rpathrepl_handler(args,parser):
     set_logging(args)
-    buildfile = './build.ninja'
+    buildfile = args.input
     replval = '/usr/lib/systemd'
     if len(args.subnargs) > 0:
-        buildfile = args.subnargs[0]
-    if len(args.subnargs) > 1:
-        replval = args.subnargs[1]
+        replval = args.subnargs[0]
     sarr = read_file_split(buildfile)
     fexpr = re.compile('\\-pie\\s+\'\\-Wl,\\-rpath,[^\']+\'')
     repv = '-pie \'-Wl,-rpath,%s\''%(replval)
@@ -123,9 +121,162 @@ def rpathrepl_handler(args,parser):
                     logging.info('[%d]m %s o[%s] n [%s]'%(lineno,m,l,cl))
                     if cl != l:
                         cont = True
-                    l = cl
+                        l = cl
             retl = l
         outs += '%s\n'%(retl)
+    write_file(outs,args.output)
+    sys.exit(0)
+    return
+
+
+class SimpleFile(object):
+    def __init__(self,fname):
+        self.fname = fname
+        self.bname = os.path.basename(fname)
+        return
+
+
+def TakeSimpleBase(elem):
+    logging.info('%s'%(elem.bname))
+    return elem.bname
+
+def compact_simple_file(s):
+    sarr = re.split('\n',s)
+    rsvec = []
+    for l in sarr:
+        l = l.rstrip('\r')
+        if len(l) == 0:
+            continue
+        rsvec.append(SimpleFile(l))
+    return rsvec
+
+
+def rpmfiles_handler(args,parser):
+    set_logging(args)
+    rs = read_file(args.subnargs[0])
+    cs = read_file(args.subnargs[1])
+    rsvec = compact_simple_file(rs)
+    csvec = compact_simple_file(cs)
+    rsvec.sort(key=TakeSimpleBase)
+    csvec.sort(key=TakeSimpleBase)
+    idx = 0
+    jdx = 0
+    hashvec = []
+    while idx < len(rsvec) and jdx < len(csvec):
+        if rsvec[idx].bname < csvec[jdx].bname:
+            logging.info('idx[%d]%s < jdx[%d] %s'%(idx,rsvec[idx].bname,jdx,csvec[jdx].bname))
+            idx += 1
+        elif rsvec[idx].bname > csvec[jdx].bname:
+            logging.info('idx[%d]%s > jdx[%d] %s'%(idx,rsvec[idx].bname,jdx,csvec[jdx].bname))
+            jdx += 1
+        else:
+            logging.info('idx[%d]jdx[%d] %s [%s]'%(idx,jdx,csvec[jdx].bname,csvec[jdx].fname))
+            hashvec.append(csvec[jdx])
+            jdx += 1
+            while jdx < len(csvec):
+                if csvec[jdx].bname != rsvec[idx].bname:
+                    break
+                logging.info('idx[%d]jdx[%d] %s [%s]'%(idx,jdx,csvec[jdx].bname,csvec[jdx].fname))
+                hashvec.append(csvec[jdx])
+                jdx += 1
+            idx += 1
+    outs = ''
+    for l in hashvec:
+        outs += '%s\n'%(l.fname)
+    write_file(outs,args.output)
+    sys.exit(0)
+    return
+
+def format_tab_line(tab,s):
+    rets = ''
+    for i in range(tab):
+        rets += '    '
+    rets += s
+    rets += '\n'
+    return rets
+
+def format_one_cp_line(srcfile,dstfile,tabs=0,retline=0):
+    logging.info('retline [%d]'%(retline))
+    rets = format_tab_line(tabs,'if [ -f "%s" ]'%(dstfile))
+    retline += 1
+    rets += format_tab_line(tabs,'then')
+    retline += 1
+    rets += format_tab_line(tabs+1,'if [ -f "%s" ]'%(srcfile))
+    retline += 1
+    rets += format_tab_line(tabs+1,'then')
+    retline += 1
+    rets += format_tab_line(tabs+2,'cp -f "%s" "%s"  || (echo "[line:%d]cp [%s] => [%s] error" >&2)'%(srcfile,dstfile,retline,srcfile,dstfile))
+    retline += 1
+    rets += format_tab_line(tabs+1,'else')
+    retline += 1
+    rets += format_tab_line(tabs+2,'echo "[line:%d]no [%s] srcfile" >&2'%(retline,srcfile))
+    retline += 1
+    rets += format_tab_line(tabs+1,'fi')
+    retline += 1
+    rets += format_tab_line(tabs,'else')
+    retline += 1
+    rets += format_tab_line(tabs+1,'echo "[line:%d]no [%s] dstfile" >&2'%(retline,dstfile))
+    retline += 1
+    rets += format_tab_line(tabs,'fi')
+    retline += 1
+    return rets,retline
+
+def append_dir_files(s,ds):
+    sarr = re.split('\n',s)
+    rsvec = []
+    for l in sarr:
+        l = l.rstrip('\r')
+        if len(l) == 0:
+            continue
+        curf = '%s/%s'%(ds,l)
+        #logging.info('cf %s ds[%s]'%(curf,ds))
+        curf = os.path.abspath(curf)
+        #logging.info('abs cf %s'%(curf))
+        rsvec.append(SimpleFile(curf))
+    return rsvec
+
+
+def formatcp_handler(args,parser):
+    set_logging(args)
+    srcdir = os.path.abspath('.')
+    if args.srcdir is not None:
+        srcdir = os.path.abspath(args.srcdir)
+    dstdir = '/'
+    if args.dstdir is not None:
+        dstdir = os.path.abspath(args.dstdir)
+    srcs = read_file(args.subnargs[0])
+    dsts = read_file(args.subnargs[1])
+    outs = format_tab_line(0,'#! /bin/sh')
+    # we set 2 because count from 1 not 0
+    retline = 2
+    srcfiles = append_dir_files(srcs,srcdir)
+    dstfiles = append_dir_files(dsts,dstdir)
+    idx = 0
+    jdx = 0
+
+    while jdx < len(dstfiles) and idx < len(srcfiles):
+        if dstfiles[jdx].bname == srcfiles[idx].bname:
+            outs += '\n'
+            retline += 1
+            s, retline= format_one_cp_line(srcfiles[idx].fname,dstfiles[jdx].fname,0,retline)
+            outs += s
+            jdx += 1
+            while jdx < len(dstfiles):
+                if dstfiles[jdx].bname != srcfiles[idx].bname:
+                    break
+                outs += '\n'
+                retline += 1
+                s , retline= format_one_cp_line(srcfiles[idx].fname,dstfiles[jdx].fname,0,retline)
+                outs += s                
+                jdx += 1
+            idx += 1
+        elif dstfiles[jdx].bname < srcfiles[idx].bname:
+            logging.error('skip jdx[%d] %s (%s)'%(jdx,dstfiles[jdx].bname,dstfiles[jdx].fname))
+            jdx += 1
+        else:
+            logging.error('skip idx[%d] %s (%s)'%(idx,srcfiles[idx].bname,srcfiles[idx].fname))
+            idx += 1
+
     write_file(outs,args.output)
     sys.exit(0)
     return
@@ -136,8 +287,16 @@ def main():
     {
         "input|i" : null,
         "output|o" : null,
-        "rpathrepl<rpathrepl_handler>##[buildfile] [rpath] to default buildfile ./build.ninja replace default /usr/lib/systemd##" : {
+        "srcdir|s" : null,
+        "dstdir|d" : null,
+        "rpathrepl<rpathrepl_handler>##[rpath] to default buildfile from input parameter replace default /usr/lib/systemd##" : {
             "$" : "*"
+        },
+        "rpmfiles<rpmfiles_handler>##rpmfiles listfiles to match rpm files##" : {
+            "$" : 2
+        },
+        "formatcp<formatcp_handler>##srcfilelist dstfilelist to copy##" : {
+            "$" : 2
         }
     }
     '''
--- config.sh
+++ config.sh
@@ -1,3 +1,13 @@
 #! /bin/bash
 
-./configure --buildtype release -Dkmod=true -Dacl=true -Dlibcryptsetup=true -Dgcrypt=true -Dlibidn2=true -Dxz=true -Dzlib=true -Dseccomp=true -Dselinux=true
+if [ -f build/build.ninja ]
+then
+./configure setup --reconfigure --buildtype release -Dkmod=true -Dacl=true -Dlibcryptsetup=true -Dgcrypt=true -Dlibidn2=true -Dxz=true -Dzlib=true -Dseccomp=true -Dselinux=true 
+else
+./configure --buildtype release -Dkmod=true -Dacl=true -Dlibcryptsetup=true -Dgcrypt=true -Dlibidn2=true -Dxz=true -Dzlib=true -Dseccomp=true -Dselinux=true 
+fi
+
+if [ -f build/build.ninja ]
+then
+	python ./config.py rpathrepl -i build/build.ninja -o build/build.ninja
+fi
